194e28f9f7fa049e27fd5794ec4c4df1c33b9f7f
Unknown <miazima@gmail.com>
2018-08-20 12:38:47 +0800

    include print content generation for simple, leftnav and post page

--- assets/js/sideNav-offset.js
+++ assets/js/sideNav-offset.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
         containerSelector: '.has-side-nav',
         innerWrapperSelector: '.sidebar__inner'
     });
+
+    $('.float-buttons').stickySidebar({
+        topSpacing: 40,
+        bottomSpacing: 40,
+        containerSelector: '.has-float-btns',
+        innerWrapperSelector: '.action__inner'
+    });
 });
