95a817d13bf03ac74e992faeb05d1b8f1ba3a60f
Preston Lim <prestonlimlianjie@gmail.com>
2018-09-10 11:21:39 +0800

    Filter resources (#16)
    * added filter by year functionality to resources; left with styling the dropdown
    
    * made resource selector buttons into a single-line overflow button row - added fade on the right for affordance but it looks a little strange now, will fix later
    
    * built filter for mobile and tablet
    
    * completed functionality for filter - will style tmr
    
    * complete styling filter dropdown
    
    * remove comments in custom scss
    
    * change resource buttons tohorizontal scroll to auto

--- assets/js/paginate-resources.js
+++ assets/js/paginate-resources.js
@@ -4,17 +4,111 @@
 let PAGE_SIZE = {{site.paginate}};
 let chunkArray;
 let currentPageIndex = 0;
+let resourceCardArray;
+let filteredArray;
 
 paginateResources();
 
 // Main function to paginate resource page
 function paginateResources() {
-  var resourceCardArray = Array.from(document.getElementsByClassName("resource-card-element")); // Convert NodeList into Array
-  chunkArray = splitPages(resourceCardArray, PAGE_SIZE);
+  resourceCardArray = Array.from(document.getElementsByClassName("resource-card-element")); // Convert NodeList into Array
+  
+  // 1. Display the years that are available
+  var earliestYear = findEarliestYear();
+  var currYear = (new Date()).getFullYear();
+  displayFilterDropdown(earliestYear, currYear);
+
+  var selectedYear = "Recent";
+  applyFilter(selectedYear);
+}
+
+function applyFilter(selectedYear) {
+  hideAllPostsAndPagination();
+
+  // Only keep posts in the selected year
+  filteredArray = extractPostsByYear(selectedYear);
+
+  chunkArray = splitPages(filteredArray, PAGE_SIZE);
   unhideChunk(currentPageIndex, currentPageIndex);
 
-  if (!resourceCardArray.length || resourceCardArray.length <= PAGE_SIZE) return;
+  var filterDropdownDesktop = document.getElementById('sgds-selector-text-desktop');
+  filterDropdownDesktop.innerHTML = selectedYear;
+
+  if (!filteredArray.length || filteredArray.length <= PAGE_SIZE) return;
   displayPagination();
+}
+
+function displayFilterDropdown(earliestYear, currYear) {
+  var yearFilterDesktop = document.getElementById('year-filter-desktop');
+  var yearFilterMobile = document.getElementById('year-filter-mobile');
+
+  for (var year = earliestYear; year <= currYear; year++) {
+    // Creating the select element for mobile view
+    var option = document.createElement("option");
+    option.value = year;
+    option.text = year;
+    yearFilterMobile.appendChild(option);
+
+    // Creating the a tags for desktop view
+    var a_element = document.createElement("a");
+    a_element.id = year;
+    a_element.classList.add("bp-dropdown-item", "padding--top--sm", "padding--bottom--none");
+    a_element.onclick = function(){
+        var closureYear = year.toString();
+        return function(){applyFilter(closureYear);}
+    }();
+
+    yearFilterDesktop.appendChild(a_element);
+    var p_element = document.createElement("p");
+    p_element.innerHTML = year;
+    a_element.appendChild(p_element);
+  }
+}
+
+function hideAllPostsAndPagination(){
+  for (let postIndex = 0; postIndex < resourceCardArray.length; postIndex ++) {
+      if (!resourceCardArray[postIndex].classList.contains("hide")) {
+        resourceCardArray[postIndex].classList.add("hide");
+      }
+  }
+
+  var paginationElement = document.getElementById("paginator-pages");
+  while (paginationElement.firstChild) {
+      paginationElement.removeChild(paginationElement.firstChild);
+  }
+
+  document.querySelector(".pagination").style.display = "none";
+}
+
+function findEarliestYear() {
+  if (resourceCardArray.length === 0) { return null; }
+
+  var earliestYear = parseInt(resourceCardArray[0].id);
+  if (resourceCardArray.length === 1) { return earliestYear; }
+
+  for (let i = 1; i < resourceCardArray.length; i++) {
+    earliestYear = (earliestYear < parseInt(resourceCardArray[i].id) ? earliestYear : parseInt(resourceCardArray[i].id));
+  }
+
+  return earliestYear;
+
+}
+
+// If the year is set to 'All', return all posts; else filter only the posts in that year
+function extractPostsByYear(year){
+  if (year === 'Recent'){
+    return resourceCardArray;
+  }
+  var tempArray = [];
+
+  for (let i = 0; i < resourceCardArray.length; i++) {
+      var post = resourceCardArray[i];
+      if (post.id === year){
+        tempArray.push(post);
+      }
+  }
+
+  return tempArray;
 }
 
 // Unhide the chunk of resource posts that user wants to see and hides the currently displayed chunk
@@ -51,7 +145,6 @@ function displayPagination() {
 
 function changePage(curr, index) {
   let prev = document.querySelector("#paginator-pages .selected-page");
-  console.log(prev);
   prev.className = "";
   prev.style.pointerEvents = "auto";
   currentPageIndex = index;
