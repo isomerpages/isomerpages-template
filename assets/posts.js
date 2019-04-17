0f02b619076a8c5d104e73fb15c835d8393d4f72
Li Yicheng <liyicheng340@gmail.com>
2019-04-17 14:48:57 +0800

    Fix putComma and remove debugging console.log

--- assets/posts.js
+++ assets/posts.js
@@ -2,19 +2,22 @@
 
 ---
 posts_json=[
+    {%- assign putComma = false -%}
     {%- for collection in site.collections -%}
-        {%- unless forloop.first -%},{%- endunless -%}
         {%- for post in collection.docs -%}
             {%- if post.permalink or post.file_url -%}
+                {%- if putComma -%},{%- endif -%}
                 {%- include post.json -%}
+                {%- assign putComma = true -%}
             {%- endif -%}
         {%- endfor -%}
     {%- endfor -%}
 
     {%- for post in site.html_pages -%}
-        ,
         {%- if post.permalink or post.file_url -%}
+            {%- if putComma -%},{%- endif -%}
             {%- include post.json -%}
+            {%- assign putComma = true -%}
         {%- endif -%}
     {%- endfor -%}
 ]
