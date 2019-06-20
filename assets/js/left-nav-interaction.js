5942cd49305b6c7c1c9e42074450e42568a6bca0
Li Yicheng <liyicheng340@gmail.com>
2019-06-20 17:44:27 +0800

    Refine leftnav logic (WIP)

--- assets/js/left-nav-interaction.js
+++ assets/js/left-nav-interaction.js
@@ -1,15 +1,15 @@
 initSecondLevelNavInteraction();
 
 function initSecondLevelNavInteraction() {
-	var secondLevelNavHeaderArray = document.querySelectorAll("li.second-level-nav-header");
+	var secondLevelNavHeaderArray = document.querySelectorAll("li.third-level-nav-header");
 	for (var index = 0; index < secondLevelNavHeaderArray.length; index++) {
 		
 		function secondLevelNavClosure() {
 			var closureIndex = index;
 			function toggleSecondLevelNavDiv() {
-				// Get the icon of the second-level-nav-header
+				// Get the icon of the third-level-nav-header
 				var secondLevelNavIcon = secondLevelNavHeaderArray[closureIndex].getElementsByTagName("I")[0];
-				var secondLevelDiv = document.getElementsByClassName("second-level-nav-div")[closureIndex];
+				var secondLevelDiv = document.getElementsByClassName("third-level-nav-div")[closureIndex];
 				if (secondLevelDiv.classList.contains("is-hidden")) {
 					secondLevelNavIcon.classList.remove("sgds-icon-chevron-down");
 					secondLevelNavIcon.classList.add("sgds-icon-chevron-up");
@@ -26,15 +26,15 @@ function initSecondLevelNavInteraction() {
 		secondLevelNavHeaderArray[index].addEventListener("click", secondLevelNavClosure());
 	}
 
-	var secondLevelNavHeaderMobileArray = document.querySelectorAll("a.second-level-nav-header-mobile");
+	var secondLevelNavHeaderMobileArray = document.querySelectorAll("a.third-level-nav-header-mobile");
 
 	for (var index = 0; index < secondLevelNavHeaderMobileArray.length; index++) {
 		function secondLevelNavMobileClosure() {
 			var closureIndex = index;
 			function toggleSecondLevelNavMobileDiv() {
-				// Get the icon of the second-level-nav-header
+				// Get the icon of the third-level-nav-header
 				var secondLevelNavMobileIcon = secondLevelNavHeaderMobileArray[closureIndex].getElementsByTagName("I")[0];
-				var secondLevelMobileDiv = document.getElementsByClassName("second-level-nav-div-mobile")[closureIndex];
+				var secondLevelMobileDiv = document.getElementsByClassName("third-level-nav-div-mobile")[closureIndex];
 				if (secondLevelMobileDiv.classList.contains("is-hidden")) {
 					secondLevelNavMobileIcon.classList.remove("sgds-icon-chevron-down");
 					secondLevelNavMobileIcon.classList.add("sgds-icon-chevron-up");
