5261e226ad0b9b358fe16dccbdb25976789c61e5
Li Yicheng <liyicheng340@gmail.com>
2019-08-02 16:17:42 +0800

    Test GA download link tracking

--- assets/js/google_analytics.js
+++ assets/js/google_analytics.js
@@ -1,17 +1,25 @@
 ---
 layout: blank
 ---
-{%- assign homepage = site.data.homepage -%}
+window.ga=window.ga || function () { (ga.q = ga.q || []).push(arguments) }; ga.l = +new Date;
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-139339739-1', 'auto', 't1');
 
-ga('create', 'UA-139339739-1', 'auto');
-ga('send', 'pageview');
+ga('t1.require', 'pageVisibilityTracker', {
+    sendInitialPageview: true,
+});
+ga('t1.require', 'outboundLinkTracker', {
+    shouldTrackOutboundLink: () => true
+});
 
 {%- if site.google_analytics -%}
-ga('create', '{{site.google_analytics}}', { 'name': 'b' });
-ga('b.send', 'pageview');
+    ga('create', '{{site.google_analytics}}', 'auto', 't2');
+
+    ga('t2.require', 'pageVisibilityTracker', {
+        sendInitialPageview: true,
+    });
+
+    ga('t2.require', 'outboundLinkTracker', {
+        shouldTrackOutboundLink: () => true
+    });
 {%- endif -%}
