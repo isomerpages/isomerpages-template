cb171f289bc1e0161bb5a2ee0571e3b72b8ecee3
LoneRifle <LoneRifle@users.noreply.github.com>
2020-10-21 11:00:18 +0800

    feat: add Linkedin Insights on request from users v1 (#186)

---
+++ assets/js/linkedin-insights.js
@@ -0,0 +1,15 @@
+---
+layout: blank
+---
+_linkedin_partner_id = "{{- site.linkedin-insights -}}";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+(function(){
+  var s = document.getElementsByTagName("script")[0];
+  var b = document.createElement("script");
+  b.type = "text/javascript";
+  b.async = true;
+  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+  s.parentNode.insertBefore(b, s);
+})();
