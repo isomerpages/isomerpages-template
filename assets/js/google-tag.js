8e6eec2f68aed659c1a36d20de65ecd396b1960f
Alexander Lee <alexander@open.gov.sg>
2023-03-24 14:58:02 +0800

    Fix: ga4

--- assets/js/google-tag.js
+++ assets/js/google-tag.js
@@ -11,5 +11,5 @@ gtag('config', 'G-25BGPGZD1C');
 
 // Set site specific ga tag
 if ('{{site.google_analytics_ga4}}' && '{{site.google_analytics_ga4}}' != "") {
-  gtag('config', '{{site.google_analytics2}}');
+  gtag('config', '{{site.google_analytics_ga4}}');
 }
