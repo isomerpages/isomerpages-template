df08806ac3bcd605f6be762b6086f6927086e340
Alexander Lee <alexander@open.gov.sg>
2023-05-25 16:32:13 +0800

    Feat: add performance stats

--- assets/js/google-tag.js
+++ assets/js/google-tag.js
@@ -4,8 +4,30 @@ layout: blank
 
 // We use layout:blank to make use of jekyll config params
 // Imported script so it isn't blocked by our CSP
+const perfObserver = new PerformanceObserver((observedEntries) => {
+  const entry =
+    observedEntries.getEntriesByType('navigation')[0]
+  console.log('pageload time: ', entry.duration)
+  // window.dataLayer.push({ pageLoadTime: 5 })
+  // if (entry.duration === 0) return
+  const parsedLoadTime = Math.round(entry.duration / 100) / 10;
+  console.log(parsedLoadTime)
+  window.dataLayer.push({ 
+    event: 'performance',
+    entryType: entry.entryType,
+    pageLoadTime: Math.round(entry.duration / 100) / 10, 
+  })
+})
+
+perfObserver.observe({
+  type: 'navigation',
+  buffered: true
+})
+
 window.dataLayer = window.dataLayer || [];
-function gtag(){window.dataLayer.push(arguments);}
+function gtag(){
+  window.dataLayer.push(arguments);
+}
 gtag('js', new Date());
 
 // Set Isomer's ga tag
