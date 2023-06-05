6d261e8ef51652de88c97df7caa77b6fcb1fa8e8
Alexander Lee <alexander@open.gov.sg>
2023-06-05 13:53:17 +0800

    Feat/Google Tag Manager (#299)
    * feat: add gtm for isomer
    
    * chore: fix inline script
    
    * Feat: add performance stats
    
    * Chore: remove unused lines

--- assets/js/google-tag.js
+++ assets/js/google-tag.js
@@ -4,8 +4,25 @@ layout: blank
 
 // We use layout:blank to make use of jekyll config params
 // Imported script so it isn't blocked by our CSP
+const perfObserver = new PerformanceObserver((observedEntries) => {
+  const entry =
+    observedEntries.getEntriesByType('navigation')[0]
+  window.dataLayer.push({ 
+    event: 'performance',
+    entryType: entry.entryType,
+    pageLoadTime: Math.round(entry.duration / 100) / 10, 
+  })
+})
+
+perfObserver.observe({
+  type: 'navigation',
+  buffered: true
+})
+
 window.dataLayer = window.dataLayer || [];
-function gtag(){window.dataLayer.push(arguments);}
+function gtag(){
+  window.dataLayer.push(arguments);
+}
 gtag('js', new Date());
 
 // Set Isomer's ga tag
