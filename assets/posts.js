71c5a4f8bb4457ca15459955bcd9e33f38b08bd8
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-10-31 11:04:02 +0800

    test

--- assets/posts.js
+++ assets/posts.js
@@ -1,23 +1,4 @@
 ---
 layout: blank
 ---
-posts_json=[
-    {%- assign putComma = false -%}
-    {%- for collection in site.collections -%}
-        {%- for post in collection.docs -%}
-            {%- if post.permalink or post.file_url -%}
-                {%- if putComma -%},{%- endif -%}
-                {%- include post.json -%}
-                {%- assign putComma = true -%}
-            {%- endif -%}
-        {%- endfor -%}
-    {%- endfor -%}
-
-    {%- for post in site.html_pages -%}
-        {%- if post.permalink or post.file_url -%}
-            {%- if putComma -%},{%- endif -%}
-            {%- include post.json -%}
-            {%- assign putComma = true -%}
-        {%- endif -%}
-    {%- endfor -%}
-]
+posts_json=[]
