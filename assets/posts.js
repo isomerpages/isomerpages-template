f3458eaf16f7079695007339081099909b059b89
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-10-31 13:37:30 +0800

    revert post_json removal

--- assets/posts.js
+++ assets/posts.js
@@ -1,4 +1,23 @@
 ---
 layout: blank
 ---
-posts_json=[]
+posts_json=[
+    {%- assign putComma = false -%}
+    {%- for collection in site.collections -%}
+        {%- for post in collection.docs -%}
+            {%- if post.permalink or post.file_url -%}
+                {%- if putComma -%},{%- endif -%}
+                {%- include post.json -%}
+                {%- assign putComma = true -%}
+            {%- endif -%}
+        {%- endfor -%}
+    {%- endfor -%}
+
+    {%- for post in site.html_pages -%}
+        {%- if post.permalink or post.file_url -%}
+            {%- if putComma -%},{%- endif -%}
+            {%- include post.json -%}
+            {%- assign putComma = true -%}
+        {%- endif -%}
+    {%- endfor -%}
+]
