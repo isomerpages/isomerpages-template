1504819734cfe3e7414ddda8748587180ae3720d
Alexander Lee <alexander@open.gov.sg>
2023-11-15 18:21:15 +0800

    Feat/datagovsg enhancement (#353)
    * chore: add specific css to handle dgsv2 table
    
    * chore: add text
    
    * chore: add sample page
    
    * feat: add skip to end button
    
    * feat: add pagination variant for mobile
    
    * fix: padding

--- assets/js/datagovsg-search.js
+++ assets/js/datagovsg-search.js
@@ -3,7 +3,8 @@ layout: blank
 ---
 'use strict';
 
-var RESULTS_PER_PAGE = 10;
+const PAGINATION_DISPLAY_RESULTS_PER_PAGE = 10; // Must be smaller than size of datagov pagination (100)
+const DATAGOV_API_RESULTS_PER_PAGE = 100; // Fixed by datagov
 var MAX_ADJACENT_PAGE_BTNS = 2;
 var MAX_ADJACENT_MOBILE_PAGE_BTNS = 1;
 var pageResults = [];
@@ -51,11 +52,21 @@ function getQueryVariable(variable) {
   }
 }
 
-function databaseSearch(searchTerm, index, searchField) {
+function getOffset(pageIndex) {
+  // Retrieves the offset required for the first item in the page
+  // pageIndex is 0-indexed
+  const firstElemIndex = pageIndex * PAGINATION_DISPLAY_RESULTS_PER_PAGE
+  const offsetRequired = Math.floor(firstElemIndex / DATAGOV_API_RESULTS_PER_PAGE) * DATAGOV_API_RESULTS_PER_PAGE
+  return offsetRequired
+}
+
+function databaseSearch(searchTerm, index, callback) {
+  const isFirstRender = pageResults.length === 0
   const resourceId = document.getElementById("resourceId").innerHTML;
+  const offset = getOffset(index)
   var data = {
     resource_id: resourceId, // the resource id
-    offset: datagovsgOffset
+    offset
   };
 
   if (searchTerm !== '') {
@@ -66,25 +77,48 @@ function databaseSearch(searchTerm, index, searchField) {
   var request = $.ajax({
     url: 'https://data.gov.sg/api/action/datastore_search',
     data: data,
-    dataType: 'json'
+    dataType: 'json',
+    success: callback
   });
 
   request.done(function (data) {
-    document.getElementById("loading-spinner").style.display = 'none';
-    hideAllPostsAndPagination();
+    datagovsgTotal = data.result.total;
+    if (isFirstRender) {
+      pageResults = Array(Math.ceil(datagovsgTotal / PAGINATION_DISPLAY_RESULTS_PER_PAGE)).fill(null);
+    } else {
+      // Delay loading spinner disappearing for first render
+      document.getElementById("loading-spinner").style.display = 'none';
+    }
+    // hideAllPostsAndPagination();
 
     // The fieldArray is the array containing the field names in the data.gov.sg table
-    fieldArray = remove(data.result.fields, ["_id", "_full_count", "rank", `rank ${searchField}`]);
-    pageResults = pageResults.concat(splitPages(data.result.records, RESULTS_PER_PAGE));
-    datagovsgTotal = data.result.total;
+    const removableFields = ["_id", "_full_count", "rank", `rank ${searchField}`]
+    fieldArray = remove(data.result.fields, removableFields);
+    const pageResultArray = splitPages(data.result.records, PAGINATION_DISPLAY_RESULTS_PER_PAGE)
+    const startingPage = offset / PAGINATION_DISPLAY_RESULTS_PER_PAGE
     const possibleSearchField = searchField || defaultField
     if (!hasPopulatedFields && possibleSearchField) {
       displaySearchFilterDropdown(fieldArray.map(item => item.id), possibleSearchField);
       hasPopulatedFields = true
     }
-    displayTable(pageResults[currentPageIndex], fieldArray);
-    if (!pageResults || pageResults.length <= 1) return;
-    displayPagination(index);
+    pageResultArray.forEach((pageData, idx) => {
+      pageResults[startingPage + idx] = pageData
+    })
+    if (isFirstRender) {
+      // Also preload the last set of pages if applicable
+      const finalPage = Math.ceil(datagovsgTotal / PAGINATION_DISPLAY_RESULTS_PER_PAGE) - 1
+      const renderDisplay = () => {
+        document.getElementById("loading-spinner").style.display = 'none';
+        displayTable(pageResults[currentPageIndex], fieldArray);
+        if (pageResults.length === 0) return;
+        displayPagination(index);
+      }
+      if (getOffset(finalPage) !== 0) {
+        databaseSearch(searchTerm, finalPage, renderDisplay)
+      } else {
+        renderDisplay()
+      }
+    }
   })
     .fail(function () { // Displays no results if the AJAX call fails
       document.getElementById("loading-spinner").style.display = 'none';
@@ -176,8 +210,10 @@ function displaySearchFilterDropdown(fields, startingField) {
 function displayPagination(index) {
   document.querySelector(".pagination").style.display = "flex";
   var pagination = document.getElementById('paginator-pages');
+  const paginationMobile = document.getElementById('paginator-pages-mobile')
+  const totalPages = Math.ceil(datagovsgTotal / PAGINATION_DISPLAY_RESULTS_PER_PAGE)
 
-  for (var i = 0; i < pageResults.length; i++) {
+  for (var i = 0; i < totalPages; i++) {
     var ele = document.createElement("span");
     var text = document.createTextNode(i + 1);
     ele.appendChild(text);
@@ -190,6 +226,16 @@ function displayPagination(index) {
     pagination.appendChild(ele);
   }
 
+  // Create page display for mobile
+  if (paginationMobile) {
+    const ele = document.createElement("span")
+    const text = document.createTextNode(`Page ${index}`)
+    ele.classList.add("is-full-height", "is-full-width", "remove-border")
+    ele.style.width = "100%"
+    ele.appendChild(text)
+    paginationMobile.appendChild(ele)
+  }
+  
   // Initialise selected page and nav arrows
   setCurrentPage(pagination.childNodes[index]);
   displayNavArrows(currentPageIndex);
@@ -201,25 +247,24 @@ function changePage(curr, index) {
     datagovsgOffset += 100;
     databaseSearch(searchTerm, index, searchField);
   }
+  // Always also look 5 pages ahead and behind
+  const forwardIndex = index + 5
+  const backwardIndex = index - 5
+  if (shouldCallAPI(forwardIndex)) {
+    databaseSearch(searchTerm, forwardIndex);
+  }
+  if (shouldCallAPI(backwardIndex)) {
+    databaseSearch(searchTerm, backwardIndex);
+  }
 
   changePageUtil(curr, index);
   displayTable(pageResults[currentPageIndex], fieldArray);
 }
 
-// Evaluates to true if we should call the datagovsg API for the 
-// next 100 rows of data
+// Evaluates to true if we should call the datagovsg API
 function shouldCallAPI(index) {
-  // Checks to make sure that the last digit of the page number is greater than 5.
-  // i.e. we should call the API for the next 100 rows if the user is currently at 
-  // page number 26.
-  // Note: the index starts from 0, so a page index of 14 corresponds to a page number of 15.
-  if (index % 10 < 4) return false;
-
-  // Makes sure that there is more data to be retrieved from the API.
-  if (datagovsgOffset + 100 > datagovsgTotal) return false;
-
-  // Makes sure that we haven't already called the API for the next 100 rows.
-  if (index * RESULTS_PER_PAGE < datagovsgOffset) return false;
-
-  return true;
+  if (pageResults[index] !== null) return false
+  if (index < 0) return false
+  if (index > datagovsgTotal / PAGINATION_DISPLAY_RESULTS_PER_PAGE) return false
+  return true
 }
