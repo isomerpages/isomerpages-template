b2796b10ffd3efe239053231259e4ebbb308333b
seaerchin <jiachin@open.gov.sg>
2024-02-07 12:47:30 +0800

    fix(ext-link): update to https

--- assets/js/floating-buttons.js
+++ assets/js/floating-buttons.js
@@ -1,13 +1,13 @@
-document.getElementById('copy-link').addEventListener("click", copyUrl);
+document.getElementById("copy-link").addEventListener("click", copyUrl);
 
-function copyUrl(){
-	var inputBox = document.getElementById('page-url');
-	if (inputBox.classList.contains("hide")){
-		inputBox.classList.remove("hide");
-	}
-	inputBox.select();
-	document.execCommand('copy');
-	inputBox.classList.add("hide");
+function copyUrl() {
+  var inputBox = document.getElementById("page-url");
+  if (inputBox.classList.contains("hide")) {
+    inputBox.classList.remove("hide");
+  }
+  inputBox.select();
+  document.execCommand("copy");
+  inputBox.classList.add("hide");
 }
 
 // on load, populate the href of the three anchor links with the full URL
@@ -15,9 +15,29 @@ function copyUrl(){
 
 const fullUrl = window.location.href;
 const fullUrlEncoded = encodeURIComponent(window.location.href);
-const titleEncoded = encodeURIComponent(document.getElementsByTagName("title")[0].innerHTML);
+const titleEncoded = encodeURIComponent(
+  document.getElementsByTagName("title")[0].innerHTML
+);
 
-document.getElementById("mail-anchor").setAttribute("href", "mailto:?Subject=" + titleEncoded + "&Body=" + fullUrlEncoded);
-document.getElementById("fb-anchor").setAttribute("href", "http://www.facebook.com/sharer.php?u=" + fullUrlEncoded);
-document.getElementById("li-anchor").setAttribute("href", "https://www.linkedin.com/sharing/share-offsite/?url=" + fullUrlEncoded + "&title=" + titleEncoded);
+document
+  .getElementById("mail-anchor")
+  .setAttribute(
+    "href",
+    "mailto:?Subject=" + titleEncoded + "&Body=" + fullUrlEncoded
+  );
+document
+  .getElementById("fb-anchor")
+  .setAttribute(
+    "href",
+    "https://www.facebook.com/sharer.php?u=" + fullUrlEncoded
+  );
+document
+  .getElementById("li-anchor")
+  .setAttribute(
+    "href",
+    "https://www.linkedin.com/sharing/share-offsite/?url=" +
+      fullUrlEncoded +
+      "&title=" +
+      titleEncoded
+  );
 document.getElementById("page-url").setAttribute("value", fullUrl);
