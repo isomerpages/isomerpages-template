1dd10052673d4a70e17258042c744a68a64b45cd
Alexander Lee <alexander@open.gov.sg>
2024-03-11 15:15:09 +0800

    fix: check integrity of scripts (#380)

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,18 +1,38 @@
-onmessage = function(event) {
-	self.importScripts('https://unpkg.com/lunr/lunr.js');
-	
+let scriptLoaded = false;
+
+onmessage = async function(event) {
+	const scriptUrl = 'https://unpkg.com/lunr/lunr.js';
+  const integrityHash = 'sha256-lDFybwXA6uKm5U3Bl3CUIoafJcrUTyQw0vt92ugMxxc=';
+	if (scriptLoaded) return
+
+	let response
+	if (!scriptLoaded) {
+		try {
+			response = await fetch(scriptUrl, { integrity: integrityHash });
+		} catch (err) {
+			console.log(err)
+			return
+		}
+	}
+	const scriptText = await response.text();
+
+	// Create a script element and append the loaded script
+	const scriptElement = new Function(scriptText);
+	scriptElement();
+
+	scriptLoaded = true
 	var documents = event.data;
 
 	var index = lunr(function () {
-	  this.ref('id');
-	  this.field('url');
-	  this.field('title');
-	  this.field('content');
-	  this.metadataWhitelist = ['position'];
+		this.ref('id');
+		this.field('url');
+		this.field('title');
+		this.field('content');
+		this.metadataWhitelist = ['position'];
 
-	  documents.forEach(function(doc) {
-	    this.add(doc);
-	  }, this);
+		documents.forEach(function(doc) {
+			this.add(doc);
+		}, this);
 	});
 
 	postMessage(JSON.stringify(index));
