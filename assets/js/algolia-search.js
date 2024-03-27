67387cba4f9f56e3d2090f98a97a6c2c4ddcefc6
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-03-27 10:11:58 +0800

    feat(egazette): add filtering

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -13,12 +13,42 @@ search.addWidgets([
   instantsearch.widgets.searchBox({
     container: "#searchbox",
     autofocus: true,
-    placeholder: "Start typing to search"
+    placeholder: "Start typing to search",
   }),
   instantsearch.widgets.poweredBy({
     container: "#poweredby",
-    theme: 'dark',
+    theme: "dark",
   }),
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-category",
+    attribute: "category",
+  }),
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-subcategory",
+    attribute: "subCategory",
+  }),
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-date",
+    attribute: "publishDate",
+
+    transformItems(items) {
+      return items.map((item) => ({
+        ...item,
+        highlighted: item.highlighted.slice(0, 10),
+        label: item.label.slice(0, 10),
+      }));
+    },
+  }),
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-number",
+    attribute: "notificationNum",
+  }),
+  // instantsearch.widgets.currentRefinements({
+  //   container: "#current-refinements",
+  // }),
+  // instantsearch.widgets.clearRefinements({
+  //   container: "#clear-refinements",
+  // }),
   instantsearch.widgets.hits({
     container: "#hits",
     templates: {
