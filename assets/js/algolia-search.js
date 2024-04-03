59e7d30b65ad72f6ca1a4d59fec7412d20e68f84
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-04-03 08:25:19 +0800

    test

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -3,6 +3,12 @@ const searchClient = algoliasearch(
   "0ba2c5f100ff5fd004415e4abbcf9b9c"
 );
 
+const PROD_URL = "https://www.egazette.gov.sg";
+const isProd = window.location.origin === PROD_URL;
+const algoliaIndexName = isProd
+  ? "ogp_egazettes_index"
+  : "staging_ogp_egazettes_index";
+
 const search = instantsearch({
   indexName: "ogp_egazettes_index",
   searchClient,
