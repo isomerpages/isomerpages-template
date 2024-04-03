ac40b8961f1b6a66fadd689afc880d89dbb37a3e
Kishore <42832651+kishore03109@users.noreply.github.com>
2024-04-03 18:12:55 +0800

    fix(year-search): not functional

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -59,11 +59,6 @@ search.addWidgets([
   instantsearch.widgets.refinementList({
     container: "#refinement-list-year",
     attribute: "publishYear",
-    searchable: true,
-  }),
-  instantsearch.widgets.refinementList({
-    container: "#refinement-list-number",
-    attribute: "notificationNum",
   }),
   instantsearch.widgets.currentRefinements({
     container: "#current-refinements",
