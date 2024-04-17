993b9566aa616ac6dea7a0e1223f36bc969d145e
Alexander Lee <alexander@open.gov.sg>
2024-04-17 17:46:49 +0800

    fix: minor copy changes

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -163,9 +163,9 @@ search.start();
 const toggleSortedVisibility = () => {
   const component = document.querySelector('#sorted-by');
   if (searchInput.value.trim() === '') {
-    component.style.display = 'block';
+    component.textContent = 'Sorted by most recent';
   } else {
-    component.style.display = 'none';
+    component.textContent = 'Sorted by relevancy';
   }
 }
 
