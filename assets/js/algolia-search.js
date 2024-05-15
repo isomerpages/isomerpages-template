bac07c93762b03d391e069652e5466aaeb5d00c1
Alexander Lee <alexander@open.gov.sg>
2024-05-15 21:57:37 +0800

    feat: change filtering behaviour

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -10,6 +10,27 @@ const isProd = window.location.origin === PROD_URL;
 //   : "staging_ogp_egazettes_index";
 const algoliaIndexName = "test_snippet_1";
 
+const categories = ["Government Gazette", "Legislative Supplements", "Other Supplements"]
+const governmentGazetteSubcategories = ["Advertisements", "Appointments", "Audited Reports", "Cessation of Service", "Corrigendum", "Death", "Dismissals", "Leave", "Bankruptcy Act Notice", "Companies Act Notice", "Notices under the Constitution", "Notices under other Acts", "Others", "Revocation", "Tenders", "Termination of Service", "Vacation of Service"]
+const legislativeSupplementsSubcategories = ["Bills Supplement", "Acts Supplement", "Revised Acts", "Subsidiary Legislation Supplement", "Revised Subsidiary Legislation"]
+const otherSupplementsSubcategories = ["Government Gazette Supplement", "Industrial Relations Supplement", "Trade Marks Supplement", "Treaties Supplement"]
+
+const categoryMapping = {
+  "Government Gazette": governmentGazetteSubcategories,
+  "Legislative Supplements": legislativeSupplementsSubcategories,
+  "Other Supplements": otherSupplementsSubcategories
+}
+
+function getSubcategories(selectedCategories) {
+  let filteredValues = [];
+
+  selectedCategories.forEach(value => {
+    filteredValues = filteredValues.concat(categoryMapping[value])
+  });
+
+  return filteredValues;
+}
+
 const search = instantsearch({
   indexName: algoliaIndexName,
   searchClient,
@@ -76,10 +97,30 @@ search.addWidgets([
     container: "#refinement-list-category",
     attribute: "category",
     limit: 20,
+    transformItems(items) {
+      const currentItemsMap = new Map(items.map(item => [item.label, item]));
+
+      // Map all possible values to their corresponding item or a default item with count 0
+      const orderedItems = categories.map(value => 
+        currentItemsMap.get(value) || { highlighted:value, value, label: value, count: 0, isRefined: false }
+      );
+
+      return orderedItems;
+    }
   }),
   instantsearch.widgets.refinementList({
     container: "#refinement-list-subcategory",
     attribute: "subCategory",
+    transformItems(items, { results }) {
+      const currentItemsMap = new Map(items.map(item => [item.label, item]));
+
+      const selectedCategories = results._state.disjunctiveFacetsRefinements.category;
+      const availableSubcategories = getSubcategories(selectedCategories)
+      const orderedItems = availableSubcategories.map(value => 
+        currentItemsMap.get(value) || { highlighted:value, value, label: value, count: 0, isRefined: false }
+      );
+      return orderedItems;
+    }
   }),
   instantsearch.widgets.refinementList({
     container: "#refinement-list-year",
